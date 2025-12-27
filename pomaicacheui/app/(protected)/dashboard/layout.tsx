d622a221c1ae17048faacb88a22096fa66e267eb
AutoCookie <fhhdhysd@gmail.com>
2025-12-27 21:58:29 +0700

    Update dashboard layout error and Analytics

--- pomaicacheui/app/(protected)/dashboard/layout.tsx
+++ pomaicacheui/app/(protected)/dashboard/layout.tsx
@@ -2,3 +2,7 @@ export const metadata = {
     title: 'Dashboard - Pomaicache',
     description: 'Access your personalized dashboard to manage your Pomaicache settings and data.',
 }
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+    return <section>{children}</section>
+}
--- pomaicacheui/app/layout.tsx
+++ pomaicacheui/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import Providers from "./provider";
 import "remixicon/fonts/remixicon.css";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata = {
   title: "Pomai Cache UI",
@@ -11,6 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className="dark">
       <body>
+        <Analytics />
         <Providers>
           {children}
         </Providers>
